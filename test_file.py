50cbdfcf9e3a895f4355bb6d3e4197659e212ab8
ramasama <faramirez.d@gmail.com>
2016-09-01 15:00:18 -0300

    imprimiendo el contenido de los archivos

--- test_file.py
+++ test_file.py
@@ -1,1 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+
+# archivos de lectura
 print('hello world')
+dataFolder = 'world-cup/'
+folders = [folder for folder in os.listdir(dataFolder) if os.path.isdir(dataFolder + folder)]
+
+# archivos para escribir
+worldcupPath = 'mundiales.csv'
+teamsPath = 'equipos.csv'
+matchesPath = 'partidos.csv'
+
+for subFolder in folders:
+    files = os.listdir(dataFolder + subFolder)
+    for file in [file for file in files if file == 'cup.txt']:
+        fullpath = dataFolder + subFolder + '/' + file
+        filereader = open(fullpath, 'r')
+        for line in filereader:
+            line = line.replace(' ', '\n')
+            print(line, end='')
+        # filereader = open(dataFolder + subFolder + )
+    break
