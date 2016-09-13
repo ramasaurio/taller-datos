59825c274ef97f10fadd57d29b8ef3215f2babc3
ramasama <faramirez.d@gmail.com>
2016-09-13 10:44:53 -0300

    ordenadas las demas carpetas

--- tallerR/R_read.R
+++ tallerR/R_read.R
@@ -1,8 +1,32 @@
+# # Data de copas del mundo
+# 
+# ## el problema hasta ahora es que no logro ordenarlos en una tabla, el separador es confuso!
+# folders<-list.files(path = "world-cup")
+# for (i in 1:20)
+# {
+#         x<-read.table(paste0(getwd(),"/world-cup/",folders[i],"/cup.txt"), sep="\t", 
+#                               encoding = "UTF-8", comment.char = "#")
+#         assign(paste0('data', folders[i]),x)
+# }
 
-folders<-list.files(path = "world-cup")
-for (i in 1:20)
-{
-        x<-read.table(paste0(getwd(),"/world-cup/",folders[i],"/cup.txt"), sep="\t", 
-                              encoding = "UTF-8", comment.char = "#")
-        assign(paste0('data', folders[i]),x)
-}
+
+# Data de jugadores (y partidos de ligas europeas) from: https://www.kaggle.com/hugomathien/soccer/
+# son datos desde 2007 para stats de jugadores
+library(RSQLite)
+library(dplyr)
+
+con <- dbConnect(SQLite(), dbname=paste0(getwd(),"/../Dataset/database.sqlite"))
+dbListTables(con)
+
+player       <- tbl_df(dbGetQuery(con,"SELECT * FROM player"))
+player_stats <- tbl_df(dbGetQuery(con,"SELECT * FROM player_stats"))
+Match        <- tbl_df(dbGetQuery(con,"SELECT * FROM Match"))
+Team         <- tbl_df(dbGetQuery(con,"SELECT * FROM Team"))
+Country      <- tbl_df(dbGetQuery(con,"SELECT * FROM Country"))
+League       <- tbl_df(dbGetQuery(con,"SELECT * FROM League"))
+SQLite       <- tbl_df(dbGetQuery(con,"SELECT * FROM sqlite_sequence"))
+
+a<-tbl_df(dbGetQuery(con,"SELECT player_name, birthday, height, weight FROM player join player_stats on 
+                     player.player_api_id = player_stats.player_api_id where player_name like 'Alexis Sanchez' GROUP BY player_name"))
+
+# Este c?digo lo saqu? del sitio Kaggle. Hay un ejemplo de como calcular el promedio de FIFA rating para cada equipo en cada partido
