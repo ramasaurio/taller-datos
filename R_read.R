9254834db6b51a1bd01451fa2f70ce114fa2e9f0
ramasama <faramirez.d@gmail.com>
2016-09-13 10:14:45 -0300

    agregada carpeta para proyecto en R

--- R_read.R
+++
@@ -1,8 +0,0 @@
-
-folders<-list.files(path = "world-cup")
-for (i in 1:20)
-{
-        x<-read.table(paste0(getwd(),"/world-cup/",folders[i],"/cup.txt"), sep="\t", 
-                              encoding = "UTF-8", comment.char = "#")
-        assign(paste0('data', folders[i]),x)
-}
