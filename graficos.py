99e56ac23b31cfadaa35b6a266eebe803e3b96b2
ramasama <faramirez.d@gmail.com>
2016-09-28 15:38:59 -0300

    grafico terminado

--- graficos.py
+++ graficos.py
@@ -1,18 +1,8 @@
-from matplotlib.collections import PathCollection
 from mpl_toolkits.basemap import Basemap
 from country import Country
+from matplotlib import colorbar
 import matplotlib.pyplot as plt
-import math
 import numpy as np
-
-
-def get_marker_color(ageplayers, league):
-    if ageplayers < 25:
-        return 'go'
-    elif ageplayers < 5.0:
-        return 'yo'
-    else:
-        return 'ro'
 
 
 def groupData(dataByCombo, leagues, seasons, banHome=False):
@@ -90,7 +80,6 @@ def run():
     eq_map.fillcontinents(color='gray')
     # eq_map.drawmapboundary()
 
-    # TODO: ELegir qué gráficos mostrar y estamos listos
     maxs = max(counts)
 
     xs = []
@@ -116,66 +105,19 @@ def run():
     # sizes = [800, 200, 200]
     pathCollection = eq_map.scatter(xs, ys, marker='o', c=colors, cmap='jet', s=sizes)
     pathCollection.set_zorder(2)
-    plt.colorbar()
+    cbar = plt.colorbar(shrink=0.9)
+    cbar.set_label("Edades")
 
     # Título
     title_string = "Jugadores extranjeros en la Premier League\n"
-    title_string += "temporadas %d a %d" % (seasons[0], seasons[-1])
-    plt.title(title_string)
-
-    plt.show()
-
-
-def colorTest():
-    # Have colormaps separated into categories:
-    # http://matplotlib.org/examples/color/colormaps_reference.html
-
-    cmaps = [('Sequential', ['Blues', 'BuGn', 'BuPu',
-                             'GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd',
-                             'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu',
-                             'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd']),
-             ('Sequential (2)', ['afmhot', 'autumn', 'bone', 'cool',
-                                 'copper', 'gist_heat', 'gray', 'hot',
-                                 'pink', 'spring', 'summer', 'winter']),
-             ('Diverging', ['BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr',
-                            'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral',
-                            'seismic']),
-             ('Qualitative', ['Accent', 'Dark2', 'Paired', 'Pastel1',
-                              'Pastel2', 'Set1', 'Set2', 'Set3']),
-             ('Miscellaneous', ['gist_earth', 'terrain', 'ocean', 'gist_stern',
-                                'brg', 'CMRmap', 'cubehelix',
-                                'gnuplot', 'gnuplot2', 'gist_ncar',
-                                'nipy_spectral', 'jet', 'rainbow',
-                                'gist_rainbow', 'hsv', 'flag', 'prism'])]
-
-    nrows = max(len(cmap_list) for cmap_category, cmap_list in cmaps)
-    gradient = np.linspace(0, 1, 256)
-    gradient = np.vstack((gradient, gradient))
-
-    def plot_color_gradients(cmap_category, cmap_list):
-        fig, axes = plt.subplots(nrows=nrows)
-        fig.subplots_adjust(top=0.95, bottom=0.01, left=0.2, right=0.99)
-        axes[0].set_title(cmap_category + ' colormaps', fontsize=14)
-
-        for ax, name in zip(axes, cmap_list):
-            ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(name))
-            pos = list(ax.get_position().bounds)
-            x_text = pos[0] - 0.01
-            y_text = pos[1] + pos[3] / 2.
-            fig.text(x_text, y_text, name, va='center', ha='right', fontsize=10)
-
-        # Turn off *all* ticks & spines, not just the ones with colormaps.
-        for ax in axes:
-            ax.set_axis_off()
-
-    for cmap_category, cmap_list in cmaps:
-        plot_color_gradients(cmap_category, cmap_list)
+    title_string += "Temporadas %d a %d" % (seasons[0], seasons[-1])
+    plt.title(title_string, fontsize=25)
 
     plt.show()
 
 
 def getMarkerSize(count, maxs):
-    maxradius = 450
+    maxradius = 500
     # maxmark = maxradius * math.sqrt(count / maxs)
     # # return count * maxmark / maxs
     return count * maxradius / maxs
